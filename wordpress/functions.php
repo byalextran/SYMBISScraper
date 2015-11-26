e73177d10d887b60a103e8b9dc14a65d6aebdf3c
Alex Tran <hello@alextran.org>
2015-11-25 20:24:10 -0600

    removed css/js query string filter

--- wordpress/functions.php
+++ wordpress/functions.php
@@ -11,38 +11,6 @@ function change_comment_link_for_disqus($link) {
 
 add_filter('get_comments_link', 'change_comment_link_for_disqus', 99);
 
-/* remove query string version numbers from css/js
-** https://rtcamp.com/tutorials/wordpress/remove-query-string-css-js-files/
-***********************************************************************/
-function remove_query_strings_from_js() {
-    global $wp_scripts;
-    if (!is_a($wp_scripts, 'WP_Scripts')) {
-        return;
-    }
-
-    foreach ($wp_scripts->registered as $handle => $script) {
-        $wp_scripts->registered[$handle]->ver = null;
-    }
-}
-
-function remove_query_strings_from_css() {
-    global $wp_styles;
-
-    if (!is_a($wp_styles, 'WP_Styles')) {
-        return;
-    }
-
-    foreach ($wp_styles->registered as $handle => $style) {
-        $wp_styles->registered[$handle]->ver = null;
-    }
-}
-
-add_action('wp_print_scripts', 'remove_query_strings_from_js', 999);
-add_action('wp_print_footer_scripts', 'remove_query_strings_from_js', 999);
-
-add_action('admin_print_styles', 'remove_query_strings_from_css', 999);
-add_action('wp_print_styles', 'remove_query_strings_from_css', 999);
-
 /* remove comment-reply js (not needed when using disqus)
 ** http://crunchify.com/try-to-deregister-remove-comment-reply-min-js-jquery-migrate-min-js-and-responsive-menu-js-from-wordpress-if-not-required/
 ***********************************************************************/
